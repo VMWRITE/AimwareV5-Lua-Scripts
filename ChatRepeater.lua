01615d7c571d2ef4cdfc5387a31ed3788cfaf33a
Ozaron <64151344+OzaronZ@users.noreply.github.com>
2020-08-22 02:32:13 +0300

    Create ChatRepeater.lua

---
+++ ChatRepeater.lua
@@ -0,0 +1,10 @@
+callbacks.Register( "DispatchUserMessage", "AutoRepeatChat ", function( msg )    
+    if msg:GetID() == 6 then
+        local index = msg:GetInt( 1 );
+        local message = msg:GetString( 4, 1 );
+		local player = entities.GetByIndex(index);
+		if player:GetTeamNumber() ~= entities.GetLocalPlayer():GetTeamNumber() then -- team check
+			client.ChatSay( message )
+		end
+    end    
+end);
