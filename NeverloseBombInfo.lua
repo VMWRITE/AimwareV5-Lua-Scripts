d6c2156b7d189e5ffd98e8810566fa15b29211c1
Ozaron <64151344+OzaronZ@users.noreply.github.com>
2020-08-26 01:08:17 +0300

    Update NeverloseBombInfo.lua

--- NeverloseBombInfo.lua
+++ NeverloseBombInfo.lua
@@ -17,43 +17,67 @@ local y = (y2 - y1) * percentage + y1
 local z = (z2 - z1) * percentage + z1 
 
 	return x, y, z 
-	
 end
 
 local function sitename(site) 
+
 local avec = entities.GetPlayerResources():GetProp("m_bombsiteCenterA")
 local bvec = entities.GetPlayerResources():GetProp("m_bombsiteCenterB")
 local sitevec1 = site:GetMins()
 local sitevec2 = site:GetMaxs()
 local site_x, site_y, site_z = lerp_pos(sitevec1.x, sitevec1.y, sitevec1.z , sitevec2.x, sitevec2.y, sitevec2.z, 0.5)
 local distance_a, distance_b = vector.Distance({site_x, site_y, site_z}, {avec.x, avec.y, avec.z}), vector.Distance({site_x, site_y, site_z}, {bvec.x, bvec.y, bvec.z})
+
 	return distance_b > distance_a and "A" or "B" 
+
 end
 
-function EventHook(Event)
+function 
+	(Event)
+
 	if Event:GetName() == "bomb_beginplant" then 
+		
 		display = true
+		planter = client.GetPlayerNameByUserID(Event:GetInt("userid")) 
+		plantingStarted = globals.CurTime() 
 		bombsite = sitename(entities.GetByIndex(Event:GetInt("site")))
+		planting = true 
+		
+		
 	end
 	
-	if Event:GetName() == "bomb_abortplant" then
+	if Event:GetName() == "bomb_abortplant" then 
+	
 		display = false
+		planting = false
+		
 	end
 	
 	if Event:GetName() == "bomb_begindefuse" then
+		
 		defusing = true
+	
 	elseif Event:GetName() == "bomb_abortdefuse" then
+	
 		defusing = false
+	
 	elseif Event:GetName() == "round_officially_ended" or Event:GetName() == "bomb_defused" or Event:GetName() == "bomb_exploded" then
+		
 		display = false
 		defusing = false
+		planting = false
+	
 	end
 	
 	if Event:GetName() == "bomb_planted" then
+
 		plantedat = globals.CurTime()
+		planting = false
+	
 	end
 	
 end
+
 
 
 callbacks.Register("Draw", "NeverLoseBombInfo", function()
@@ -63,7 +87,7 @@ if entities.FindByClass("CPlantedC4")[1] ~= nil then
 	local bombtimer = math.floor((plantedat - globals.CurTime() + Bomb:GetProp("m_flTimerLength")) * 10) / 10
 	draw.SetFont(font)
 	draw.Color(0, 150, 255, 50)
-	draw.RoundedRectFill( 2, h/2-48, 268, h/2+3, 3, true, true, true, true )
+	draw.RoundedRectFill( 2, h/2.2, 268, h/2+3, 3, true, true, true, true )
     draw.Color(5,14,26, 255)
     draw.FilledRect(5, h/2-45, 265, h/2)
 	draw.Color(255,255,255,255)
@@ -127,3 +151,12 @@ function BombDamage(Bomb)
 	
 end
 callbacks.Register("FireGameEvent", "EventHookB", EventHook);
+client.AllowListener( "bomb_beginplant" );
+client.AllowListener( "bomb_abortplant" );
+client.AllowListener( "bomb_begindefuse" );
+client.AllowListener( "bomb_abortdefuse" ); 
+client.AllowListener( "bomb_defused" );
+client.AllowListener( "bomb_exploded" );
+client.AllowListener( "round_officially_ended" );
+client.AllowListener( "bomb_planted" );
+
